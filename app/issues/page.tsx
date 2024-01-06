e0b71f6199a9fa48ddd35a99a230dcf760055d0a
Tobechi Nwachukwu <tobechid6@gmail.com>
2024-01-06 15:41:30 -0500

    styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,10 @@
-import { Button, Table } from '@radix-ui/themes'
+import { Table } from '@radix-ui/themes'
 import React from 'react'
 import prisma from '@/prisma/client'
 import IssueStatusBadge from '../components/IssueStatusBadge'
 import delay from 'delay'
+import Link from '../components/Link'
 import IssueActions from './IssueActions'
-import Link from 'next/link'
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany()
