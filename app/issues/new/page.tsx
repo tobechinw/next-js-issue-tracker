d1bbe4b297f5dadc6c85efce1549506e3af7c283
Tobechi Nwachukwu <tobechid6@gmail.com>
2024-01-06 13:54:26 -0500

    client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,14 +7,16 @@ import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import ErrorMessage from './ErrorMessage';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-    const { register, control, handleSubmit } = useForm<IssueForm>()
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    })
     const router = useRouter();
     const [error, setError] = useState('')
 
@@ -35,11 +37,13 @@ const NewIssuePage = () => {
             <TextField.Root >
                 <TextField.Input placeholder='Title' {...register('title')} />
             </TextField.Root>
+            { errors.title && <ErrorMessage error={errors.title.message!} />}
             <Controller 
                 name='description'
                 control={control}
                 render={( { field } ) => <SimpleMDE placeholder='Description' {...field} />}
             />
+            { errors.description && <ErrorMessage error={errors.description.message!} />}
             <Button>Submit New Issue</Button>
         </form>
     </div>
