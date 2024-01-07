ff6301d61deccd1252b7a179a14b90bc5ecf42f3
Tobechi Nwachukwu <tobechid6@gmail.com>
2024-01-07 02:27:27 -0500

    added delete button

--- app/issues/[id]/EditIssueButton.tsx
+++ app/issues/[id]/EditIssueButton.tsx
@@ -3,7 +3,11 @@ import { Button } from '@radix-ui/themes'
 import Link from 'next/link'
 import React from 'react'
 
-const EditIssueButton = ({ issueId }: { issueId: number}) => {
+interface Props {
+  issueId: number
+}
+
+const EditIssueButton = ({ issueId }: Props ) => {
   return (
     <Button>
         <Pencil2Icon/>
