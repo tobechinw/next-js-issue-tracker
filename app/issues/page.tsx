78edeea9d9a72263fda35042579d44616f05ec8b
Tobechi Nwachukwu <tobechid6@gmail.com>
2024-01-08 11:44:46 -0500

    filtering issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,47 +1,64 @@
-import { Table } from '@radix-ui/themes'
-import React from 'react'
-import prisma from '@/prisma/client'
-import IssueStatusBadge from '../components/IssueStatusBadge'
-import Link from '../components/Link'
-import IssueActions from './IssueActions'
+import { Table } from '@radix-ui/themes';
+import React from 'react';
+import prisma from '@/prisma/client';
+import IssueStatusBadge from '../components/IssueStatusBadge';
+import Link from '../components/Link';
+import IssueActions from './IssueActions';
+import { Status } from '@prisma/client';
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany()
-
-  return (
-    <div>
-      <IssueActions/>
-      <Table.Root variant='surface'>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map(issue => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>
-                  {issue.title}
-                </Link>
-                <div className='block md:hidden'>
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-    </div>
-  )
+interface Props {
+    searchParams: { status: Status };
 }
 
-export const dynamic = 'force-dynamic'
+const IssuesPage = async ({ searchParams }: Props) => {
+    const statuses = Object.values(Status);
+    const status = statuses.includes(searchParams.status)
+        ? searchParams.status
+        : undefined;
+    const issues = await prisma.issue.findMany({
+        where: { status }
+    });
 
-export default IssuesPage
+    return (
+        <div>
+            <IssueActions />
+            <Table.Root variant="surface">
+                <Table.Header>
+                    <Table.Row>
+                        <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell className="hidden md:table-cell">
+                            Status
+                        </Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell className="hidden md:table-cell">
+                            Created
+                        </Table.ColumnHeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {issues.map((issue) => (
+                        <Table.Row key={issue.id}>
+                            <Table.Cell>
+                                <Link href={`/issues/${issue.id}`}>
+                                    {issue.title}
+                                </Link>
+                                <div className="block md:hidden">
+                                    <IssueStatusBadge status={issue.status} />
+                                </div>
+                            </Table.Cell>
+                            <Table.Cell className="hidden md:table-cell">
+                                <IssueStatusBadge status={issue.status} />
+                            </Table.Cell>
+                            <Table.Cell className="hidden md:table-cell">
+                                {issue.createdAt.toDateString()}
+                            </Table.Cell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table.Root>
+        </div>
+    );
+};
+
+export const dynamic = 'force-dynamic';
+
+export default IssuesPage;
