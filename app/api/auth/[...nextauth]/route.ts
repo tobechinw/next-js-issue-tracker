6990e0e8de4d729653bc79aefb0797b28344a42f
Tobechi Nwachukwu <tobechid6@gmail.com>
2024-01-07 22:23:05 -0500

    added middleware for routes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,7 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import CredentialsProvider from "next-auth/providers/credentials"
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import prisma from "@/prisma/client"
-import bcrypt from 'bcrypt'
+import authOptions from "@/app/auth/authOptions"
 
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-    })
-  ],
-  session: {
-    strategy: 'jwt'
-  }
-})
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
