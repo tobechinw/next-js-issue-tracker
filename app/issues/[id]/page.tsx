38a4e33bcbbb86d78f04799d57d07b175014b8b6
Tobechi Nwachukwu <tobechid6@gmail.com>
2024-01-08 13:58:42 -0500

    add metadata for SEO

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -5,33 +5,43 @@ import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
 import DeleteIssueButton from './DeleteIssueButton';
 import AssigneeSelect from './AssigneeSelect';
+import { title } from 'process';
 
 interface Props {
-  params: { id: string };
+    params: { id: string };
 }
 
-const pages = async  ({ params }: Props) => {
-  const issue = await prisma.issue.findUnique({ 
-    where: { id: parseInt(params.id) }
-  })
+const pages = async ({ params }: Props) => {
+    const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(params.id) }
+    });
 
+    if (!issue) notFound();
 
-  if(!issue) notFound()
+    return (
+        <Grid gap="5" columns={{ initial: '1', sm: '5' }}>
+            <Box className="md:col-span-4">
+                <IssueDetails issue={issue} />
+            </Box>
+            <Box>
+                <Flex direction={'column'} gap={'4'}>
+                    <AssigneeSelect issue={issue} />
+                    <EditIssueButton issueId={issue.id} />
+                    <DeleteIssueButton issueId={issue.id} />
+                </Flex>
+            </Box>
+        </Grid>
+    );
+};
 
-  return (
-    <Grid gap="5" columns={{initial: "1", sm: "5"}}>
-      <Box className='md:col-span-4'>
-        <IssueDetails issue={issue} />
-      </Box>
-      <Box>
-        <Flex direction={'column'} gap={'4'}>
-          <AssigneeSelect issue={issue}/>
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
-    </Grid>
-  )
+export async function generateMetadata({ params }: Props) {
+    const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(params.id) }
+    });
+    return {
+        title: issue?.title,
+        description: `Details of issue ${issue?.id}`
+    };
 }
 
-export default pages
+export default pages;
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import IssueChart from './IssueChart';
 import IssueSummary from './IssueSummary';
 import prisma from '@/prisma/client';
 import LatestIssues from './LatestIssues';
+import { Metadata } from 'next';
 
 export default async function Home() {
     const open = await prisma.issue.count({ where: { status: 'OPEN' } });
@@ -29,3 +30,8 @@ export default async function Home() {
         </Grid>
     );
 }
+
+export const metadata: Metadata = {
+    title: 'Issue Tracker - Dashboard',
+    description: 'View a summary of project issues and render a dashboard'
+};
