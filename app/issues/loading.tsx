67128a96e7adf29de93f1f8e8fce616d81ea2516
Tobechi Nwachukwu <tobechid6@gmail.com>
2024-01-08 11:33:51 -0500

    added filter component and prettier config

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,41 +1,47 @@
-import { Table } from '@radix-ui/themes'
-import React from 'react'
-import Skeleton from '@/app/components/Skeleton'
-import IssueActions from './IssueActions'
+import { Table } from '@radix-ui/themes';
+import React from 'react';
+import Skeleton from '@/app/components/Skeleton';
+import IssueActions from './IssueActions';
 
 const LoadingIssuesPage = () => {
-    const issues = [1, 2, 3, 4, 5]
+    const issues = [1, 2, 3, 4, 5];
 
     return (
         <div>
-            <IssueActions/>
-            <Table.Root variant='surface'>
+            <IssueActions />
+            <Table.Root variant="surface">
                 <Table.Header>
-                <Table.Row>
-                    <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
-                </Table.Row>
+                    <Table.Row>
+                        <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell className="hidden md:table-cell">
+                            Status
+                        </Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell className="hidden md:table-cell">
+                            Created
+                        </Table.ColumnHeaderCell>
+                    </Table.Row>
                 </Table.Header>
                 <Table.Body>
-                {issues.map(issue => (
-                    <Table.Row key={issue}>
-                    <Table.Cell>
-                        <Skeleton />
-                        <div className='block md:hidden'>
-                        <Skeleton />
-                        </div>
-                    </Table.Cell>
-                    <Table.Cell className='hidden md:table-cell'>
-                        <Skeleton />
-                    </Table.Cell>
-                    <Table.Cell className='hidden md:table-cell'><Skeleton /></Table.Cell>
-                    </Table.Row>
-                ))}
+                    {issues.map((issue) => (
+                        <Table.Row key={issue}>
+                            <Table.Cell>
+                                <Skeleton />
+                                <div className="block md:hidden">
+                                    <Skeleton />
+                                </div>
+                            </Table.Cell>
+                            <Table.Cell className="hidden md:table-cell">
+                                <Skeleton />
+                            </Table.Cell>
+                            <Table.Cell className="hidden md:table-cell">
+                                <Skeleton />
+                            </Table.Cell>
+                        </Table.Row>
+                    ))}
                 </Table.Body>
             </Table.Root>
         </div>
-    )
-}
+    );
+};
 
-export default LoadingIssuesPage
+export default LoadingIssuesPage;
